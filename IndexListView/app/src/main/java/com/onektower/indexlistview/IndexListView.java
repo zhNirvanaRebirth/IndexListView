b387e7abf83eeaba81ce9c3de82cff434ac32a84
zhouhang <2501213087@qq.com>
2016-12-01 11:04:13 +0800

    add ListView theme style

---
+++ IndexListView/app/src/main/java/com/onektower/indexlistview/IndexListView.java
@@ -0,0 +1,28 @@
+package com.onektower.indexlistview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ListView;
+
+/**
+ * Created by zhwilson on 2016/12/1.
+ * 模仿手机联系人界面的listview
+ */
+public class IndexListView extends ListView {
+    //需要的几个View
+    private View mGroupView;//左侧分组View
+    private View mIndexView;//索引栏View
+
+    public IndexListView(Context context) {
+        super(context);
+    }
+
+    public IndexListView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public IndexListView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
